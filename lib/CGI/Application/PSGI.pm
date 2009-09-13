50c847c1316315444c618b60fd96700bee6fa301
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-09-13 17:17:01 +0900

    make it a runner to do roughly the same as MyAapp->new->run

--- lib/CGI/Application/PSGI.pm
+++ lib/CGI/Application/PSGI.pm
@@ -4,38 +4,56 @@ use strict;
 use 5.008_001;
 our $VERSION = '0.01';
 
-use IO::File;
+use CGI::PSGI;
 
 sub run {
-    my($class, $app, $env) = @_;
+	my $class = shift;
+	my $self = shift;
+	my $q = $self->query();
 
-    local *ENV = $env;
-    local *STDIN  = $env->{'psgi.input'};
-    local *STDERR = $env->{'psgi.errors'};
+	my $rm_param = $self->mode_param();
 
-    # ripped from HTTP::Request::AsCGI
-    my $stdout = IO::File->new_tmpfile;
-    open my $old_stdout, '>&'. STDOUT->fileno
-        or die "Can't dup stdout: $!";
-    open STDOUT, '>&='. $stdout->fileno
-        or die "Can't open stdout: $!";
-    binmode $stdout;
-    binmode STDOUT;
+	my $rm = $self->__get_runmode($rm_param);
 
-    $app->run;
+	# Set get_current_runmode() for access by user later
+	$self->{__CURRENT_RUNMODE} = $rm;
 
-    STDOUT->flush
-        or die "Can't flush stdout: $!";
-    open STDOUT, '>&'. fileno($old_stdout)
-        or die "Can't restore stdout: $!";
+	# Allow prerun_mode to be changed
+	delete($self->{__PRERUN_MODE_LOCKED});
 
-    sysseek( $stdout, 0, SEEK_SET )
-        or die "Can't seek stdout: $!";
+	# Call PRE-RUN hook, now that we know the run mode
+	# This hook can be used to provide run mode specific behaviors
+	# before the run mode actually runs.
+ 	$self->call_hook('prerun', $rm);
 
-    # FIXME: read status and headers now
+	# Lock prerun_mode from being changed after cgiapp_prerun()
+	$self->{__PRERUN_MODE_LOCKED} = 1;
 
-    return [ 200, [ 'Content-Type' => "text/html" ], $stdout ];
+	# If prerun_mode has been set, use it!
+	my $prerun_mode = $self->prerun_mode();
+	if (length($prerun_mode)) {
+		$rm = $prerun_mode;
+		$self->{__CURRENT_RUNMODE} = $rm;
+	}
+
+	# Process run mode!
+	my $body = $self->__get_body($rm);
+
+	# Support scalar-ref for body return
+	$body = $$body if ref $body eq 'SCALAR';
+
+	# Call cgiapp_postrun() hook
+	$self->call_hook('postrun', \$body);
+
+	# Set up HTTP headers
+	my($status, $headers) = $self->_send_headers();
+
+	# clean up operations
+	$self->call_hook('teardown');
+
+	return [ $status, $headers, [ $body ] ];
 }
+
 
 1;
 __END__
@@ -54,20 +72,25 @@ CGI::Application::PSGI - PSGI Adapter for CGI::Application
   package WebApp;
   use base qw(CGI::Application);
 
-  # Nothing needs to be changes
+  # Nothing needs to be changed
 
-  ### server
   use CGI::Application::PSGI;
   use WebApp;
 
-  my $webapp = WebApp->new;
-  my $app = sub { CGI::Application::PSGI->run($webapp, @_) };
+  my $app = sub {
+      my $env = shift;
+
+      my $webapp = WebApp->new;
+      local *ENV = $env;
+
+      CGI::Application::PSGI->run($webapp);
+  };
 
   # run $app with a PSGI implementation
 
 =head1 DESCRIPTION
 
-CGI::Applicaition is a runner to run existent CGI::Application web application as a PSGI application.
+CGI::Application::PSGI is a runner to run existent CGI::Application web application as a PSGI application.
 
 =head1 AUTHOR
 
@@ -79,5 +102,7 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
+
+L<CGI::PSGI> L<CGI::Application::PSGI>
 
 =cut
